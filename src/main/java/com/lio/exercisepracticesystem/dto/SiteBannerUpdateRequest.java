8b9be64d5b4590860a514f535adefc05fc2bcb5a
lio <lioyanysd@gmail.com>
2026-05-28 17:07:03 +0800

    删除

--- src/main/java/com/lio/exercisepracticesystem/dto/SiteBannerUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.lio.exercisepracticesystem.dto;
-
-import lombok.Data;
-
-@Data
-public class SiteBannerUpdateRequest {
-    private String homeBannerText;
-    private Boolean homeBannerEnabled;
-}
--- src/main/java/com/lio/exercisepracticesystem/entity/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.lio.exercisepracticesystem.entity;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
